6460a8210e0fc015c16c55eaa6edd4fc4c042d67
Kiran Banmala <kiranbanmala3@gmail.com>
2025-07-20 22:51:12 +0545

    Update API base URL to production endpoint for deployment

--- javascript/api/api.js
+++ javascript/api/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:3000/api';
-// const API_BASE_URL = 'https://measurement-sheets-backend.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://measurement-sheets-backend.onrender.com/api';
 
 class ProjectAPI {
   constructor() {
